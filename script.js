646274c48a03245a75755617bb06d0e24d907b78
meetelijah <igundor@wgu.edu>
2022-08-27 15:00:07 -0400

    Basic Add Text Functionality

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World!");
+
+//prevents default form submission behavior -- no reload upon enter
+var form = document.getElementById("myForm");
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
+
+//access the todo list
+toDoList = document.querySelector('.to-do-list');
+
+//access todo input field
+toDoInput = document.getElementById("add-todo");
+//creates new html todo
+function newItem(x) {
+
+    //creates new h6 tag
+    let header = document.createElement('h6');
+    // populates h6 text
+    header.innerHTML = x.value;
+    //adds id
+    header.id = '5';
+    //appends to todolist
+    return header
+}
+
+//adds todo to web page
+function addTodo(event) {
+    if (event.key === "Enter" && toDoInput.value) {
+        let item = newItem(toDoInput);
+        toDoList.appendChild(item);
+    }
+}
+
+//event listener for "enter" press
+document.body.addEventListener('keydown', addTodo);
+
